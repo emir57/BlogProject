6f1f82b30123e420ad804863a855f2a097379fd6
emir57 <emir.gurbuz06@hotmail.com>
2021-10-31 17:09:57 +0300

    Update Home Page

--- BlogMvcApp/BlogMvcApp/Controllers/HomeController.cs
+++ BlogMvcApp/BlogMvcApp/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 ﻿using BlogMvcApp.Models.EntityFramework.Context;
+using BlogMvcApp.Models.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,9 +14,25 @@ namespace BlogMvcApp.Controllers
     {
         private BlogContext context = new BlogContext();
         // GET: Home
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View(context.Blogs.ToList());
+            var blogs = await context.Blogs
+                .Select(a => new BlogModel
+                {
+                    Id = a.Id,
+                    Title = a.Title.Length > 100 ? a.Title.Substring(0, 100) + "..." : a.Title,
+                    Description = a.Description,
+                    Picture = a.Picture,
+                    AddingDate = a.AddingDate,
+                    IsOk=a.IsOk,
+                    IsHomePage=a.IsHomePage
+                })
+                .Where(a => a.IsOk == true && a.IsHomePage == true)
+                .OrderByDescending(a => a.AddingDate)
+                .ToListAsync();
+            ViewBag.categories = await context.Categories.ToListAsync();
+
+            return View(blogs);
         }
     }
 }
---
+++ BlogMvcApp/BlogMvcApp/Models/ViewModels/BlogModel.cs
@@ -0,0 +1,20 @@
+﻿using BlogMvcApp.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogMvcApp.Models.ViewModels
+{
+    public class BlogModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Picture { get; set; }
+        public DateTime AddingDate { get; set; }
+        public bool IsOk { get; set; }
+        public bool IsHomePage { get; set; }
+
+    }
+}
