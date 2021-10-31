c2e5dcc480c1e0da59296230604de53c67f17e38
emir57 <emir.gurbuz06@hotmail.com>
2021-10-31 14:53:23 +0300

    Adding Entities
    Category and Blog

---
+++ BlogMvcApp/BlogMvcApp/Models/Entities/Blog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlogMvcApp.Models.Entities
+{
+    public class Blog
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Picture { get; set; }
+        public string Icerik { get; set; }
+        public DateTime AddingDate { get; set; }
+        public bool IsOk { get; set; }
+        public bool IsHomePage { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ BlogMvcApp/BlogMvcApp/Models/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlogMvcApp.Models.Entities
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+
+        public List<Blog> Blogs { get; set; }
+    }
+}
